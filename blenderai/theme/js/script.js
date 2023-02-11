f05c9229107cf5b9db428eea5239e47fbc57134e
BlenderAI <125104204+BlenderAI@users.noreply.github.com>
2023-02-11 17:51:44 +0000

    done

---
+++ blenderai/theme/js/script.js
@@ -0,0 +1,80 @@
+/**
+ * WEBSITE: https://themefisher.com
+ * TWITTER: https://twitter.com/themefisher
+ * FACEBOOK: https://www.facebook.com/themefisher
+ * GITHUB: https://github.com/themefisher/
+ */
+
+(function ($) {
+	'use strict';
+
+	// Preloader js    
+	$(window).on('load', function () {
+		$('.preloader').fadeOut(100);
+	});
+
+
+	// navfixed
+	$(window).scroll(function () {
+		if ($('.navigation').offset().top > 50) {
+			$('.navigation').addClass('nav-bg');
+		} else {
+			$('.navigation').removeClass('nav-bg');
+		}
+	});
+
+	// masonry
+	$('.masonry-wrapper').masonry({
+		columnWidth: 1
+	});
+
+	// clipboard
+	var clipInit = false;
+	$('code').each(function () {
+		var code = $(this),
+			text = code.text();
+		if (text.length > 2) {
+			if (!clipInit) {
+				var text, clip = new ClipboardJS('.copy-to-clipboard', {
+					text: function (trigger) {
+						text = $(trigger).prev('code').text();
+						return text.replace(/^\$\s/gm, '');
+					}
+				});
+				clipInit = true;
+			}
+			code.after('<span class="copy-to-clipboard">copy</span>');
+		}
+	});
+	$('.copy-to-clipboard').click(function () {
+		$(this).html('copied');
+	});
+
+
+	// match height
+	$(function () {
+		$('.match-height').matchHeight({
+			byRow: true,
+			property: 'height',
+			target: null,
+			remove: false
+		});
+	});
+
+	// search
+	$('#search-by').keyup(function () {
+		if (this.value) {
+			$(this).addClass('active')
+		} else {
+			$(this).removeClass('active')
+		}
+	})
+
+	// Accordions
+	$('.collapse').on('shown.bs.collapse', function () {
+		$(this).parent().find('.ti-plus').removeClass('ti-plus').addClass('ti-minus');
+	}).on('hidden.bs.collapse', function () {
+		$(this).parent().find('.ti-minus').removeClass('ti-minus').addClass('ti-plus');
+	});
+
+})(jQuery);
